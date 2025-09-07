ebd2d926bcf831290669e642e99575ac29a5a947
steffjenl <stephan@monkeysoft.nl>
2025-09-07 13:44:38 +0200

    Minimal Log Level Default to 400

--- config/sitesmonkey.php
+++ config/sitesmonkey.php
@@ -10,4 +10,5 @@ return [
         'model' => env('SITESMONKEY_AUTH_MODEL', '\App\Models\User'),
         'route_after_login' => env('SITESMONKEY_AUTH_ROUTE_AFTER_LOGIN', 'dashboard'),
     ],
+    'minimal_log_level' => env('SITESMONKEY_MINIMAL_LOG_LEVEL', 400)
 ];
